b5d2c302ce1f5a86f1896f3cec5d306f11cb9570
Arnold Krille <arnold@arnoldarts.de>
2014-11-24 23:51:50 +0100

    Trying to save references to the functions and record the calls to the functions.
    If only I knew ruby...

--- lib/rroar/recorder.rb
+++ lib/rroar/recorder.rb
@@ -6,9 +6,34 @@ module Rroar
     # Record all function calls with their arguments and return values
     #
 
+    #print self.class.instance_methods(false)
+
     def initialize
       @recording = false
       @recorded = []
+      @myfuncs = {}
+      #print self.class.instance_methods(false)
+      self.class.instance_methods(false).each do |method|
+        #print "Should remove method #{method}"
+        @myfuncs[method] = self.class.instance_method(method)
+        self.class.class_eval do undef_method(method) end
+        #self.class.undef_method method
+      #  self.instance_eval "def rr_#{method}; do print 'Should call #{method}' end"
+      #  print self.class.instance_method(method).parameters
+      #  #self.class.module_eval("@@rr_#{method} = self.instance_method(:#{method})")
+      #  #self.class.class_eval("@@rr_#{method}.bind(self)")
+      #  #self.class.class_eval("def #{method} { rr_#{method} }")
+      end
+      #print self.class.instance_methods(false)
+    end
+
+    def method_missing(m, *args, &block)
+      print "method_missing: Called #{m} with #{args.inspect}\n"
+      print " myfuncs is #{@myfuncs}\n"
+      if @myfuncs.has_key?(m)
+        #@myfuncs[m](*args, &block)
+        print "Should record this call!"
+      end
     end
 
     def start_recording
--- spec/rroar/recorder_spec.rb
+++ spec/rroar/recorder_spec.rb
@@ -6,6 +6,10 @@ class MyClass
     'Hello'
   end
 
+  def hello_with_args(*args)
+    "Hello #{args}"
+  end
+
   include Rroar::Recorder
 end
 
@@ -18,6 +22,7 @@ describe 'Rroar.Recorder' do
   end
 
   it 'has keeps references to the original functions of a class' do
+    #print mc.methods false
     expect(mc).to respond_to('rr_hello')
   end
 
@@ -32,5 +37,11 @@ describe 'Rroar.Recorder' do
     expect(recording).to eq([])
   end
 
-  it 'records calls to all functions'
+  it 'records calls to all functions' do
+    mc.bla
+    mc.hello
+    mc.hello_with_args('bla')
+    recording = mc.get_recording
+    expect(recording).not_to eq([])
+  end
 end
